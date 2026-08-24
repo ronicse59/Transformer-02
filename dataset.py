c54b16f65e84449880c9ea8c3529bda8bdcba3a6
Md Siddiqur Rahman <md-siddiqur.rahman@sogeti.com>
2026-08-24 02:44:27 +0200

    error correction

--- dataset.py
+++ dataset.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 
 class BilingualDataset(Dataset):
 
-    def __init__(self, ds, trokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
+    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
         super().__init__()
 
         self.ds = ds
