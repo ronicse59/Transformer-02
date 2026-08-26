c1c4ce68cf85a5a16dcf2eb60dcee2b8ac451600
Md Siddiqur Rahman <md-siddiqur.rahman@sogeti.com>
2026-08-26 19:28:00 +0200

    Continuing the training loop

--- train.py
+++ train.py
@@ -3,12 +3,20 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
 from dataset import BilingualDataset, causal_mask
+from model import build_transformer
+
+from config import get_weights_file_path, get_config
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
+
+
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
 
 from pathlib import Path
 
@@ -63,4 +71,47 @@ def get_ds(config):
 
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
+    return model
 
+def train_model(config):
+    # Define the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device {device}')
+
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    # Tensorboard
+    write = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+
+    initial_epoch = 0
+    global_step = 0
+    if config['preload']:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print(f"Preload model {model_filename}")
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id(['PAD']), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        model.train()
+        batch_interator = tqdm(train_dataloader, desc=f'Processing epoch {epoch:02d}')
+        for batch in batch_interator:
+
+            encoder_input = batch['encoder_input'].to(device) # (B, Seq_Len)
+            decoder_input = batch['decoder_input'].to(device) # (B, Seq_Len)
+            encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, Seq_Len)
+            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, Seq_Len, Seq_Len)
+
+            # Run the tensors through the transformer
